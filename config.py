70c02989c59e0410093c80a31dbb48ecf0ba7137
Utkrashh <utkrashh@gmail.com>
2025-03-17 11:43:47 +0530

    updated config

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 import os
 
+# Google Cloud Project ID
+PROJECT_ID = "teak-clarity-453916-a5"
+
+# BigQuery Dataset and Table Name
+DATASET_ID = "my_learning_dataset"
+TABLE_NAME = "my_first_table"
+
+# Construct Full BigQuery Table ID
+TABLE_ID = f"{PROJECT_ID}.{DATASET_ID}.{TABLE_NAME}"
+
 # Directory containing the data files
 DATA_DIRECTORY = os.path.join(os.getcwd(), "data")
-
-# BigQuery Table ID in the format `project.dataset.table`
-TABLE_ID = "teak-clarity-453916-a5.my_learning_dataset.my_first_table"
 
 # Log file path
 LOG_FILE = os.path.join(os.getcwd(), "logs", "etl.log")
